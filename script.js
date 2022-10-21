41a91c9e0f1f2b763aec13a1d24aafe205ec145b
Madi Simpson <madibyee@gmail.com>
2022-10-21 10:32:30 -0700

    styles/index/script: add a "copied text" snackbar and better tooltips
    thank u liyah for the help with the snackbars <3

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function displayTooltip(elementId) {
+    document.getElementById(elementId).style.visibility = "visible"
+}
+
+function hideTooltip(elementId) {
+    document.getElementById(elementId).style.visibility = "hidden"
+}
+
+function snackbar(element) {
+    var x = document.getElementById(element);
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
 function copyText() {
     navigator.clipboard.writeText("madibye ✿#5040");
 }
